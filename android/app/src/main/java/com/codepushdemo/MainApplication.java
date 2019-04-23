56926cd1c9fd43ae6cf6236cd8a6447593ed2678
wiseqingyang <wiseqingyang@163.com>
2019-04-23 21:39:51 +0800

    添加react-native-fs

--- android/app/src/main/java/com/codepushdemo/MainApplication.java
+++ android/app/src/main/java/com/codepushdemo/MainApplication.java
@@ -3,6 +3,7 @@ package com.codepushdemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );
     }
